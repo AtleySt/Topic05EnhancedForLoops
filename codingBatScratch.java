074b69003808cc1b934ba1b6ef85d6b329cba914
Atley St. Vrain <atleystvrain@gmail.com>
2023-02-15 12:06:42 -0600

    sum13 | In Progress

--- codingBatScratch.java
+++ codingBatScratch.java
@@ -36,4 +36,19 @@ public class codingBatScratch {
             return false;
         }
     }
+
+    // Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
+    // sum13([1, 2, 2, 1]) → 6
+    // sum13([1, 1]) → 2
+    // sum13([1, 2, 2, 1, 13]) → 6
+
+    public static int sum13(int[] nums) {
+        int sum = 0;
+        for (int item = 1; item < nums.length; item++) {
+            if (nums[item] != 13 && nums[item-1] != 13) {
+                sum = sum +nums[item];
+            }
+        }
+        return sum;
+    }
 }
